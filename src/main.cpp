fb4f91cdc5a1a22afb066e179fea2ab808aa7118
psolblanc <p_solblanc@hotmail.com>
2022-06-13 13:41:03 +0200

    Added naked logger infrastructura (no wrapper for the moment)

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include "spdlog/spdlog.h"
 
 int main() {
 
-  std::cout << "Helloaaaa." << std::endl;
+  std::cout << "Hello allread." << std::endl;
+  spdlog::info("Hello, {}!", "World");
   return 0;
 }
